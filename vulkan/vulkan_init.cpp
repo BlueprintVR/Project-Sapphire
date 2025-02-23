e8581180d1d05b5251a0299ed877cf7fdcfb7680
boyuia <dodylan190@gmail.com>
2025-02-24 07:56:17 +1100

    Create vulkan_init.cpp

---
+++ vulkan/vulkan_init.cpp
@@ -0,0 +1,13 @@
+VkInstanceCreateInfo createInfo = {};
+createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+
+// Get OpenVR's required Vulkan extensions
+std::vector<const char*> extensions;
+extensions.push_back(VRCompositor()->GetVulkanInstanceExtensionsRequired());
+
+// Pass extensions to Vulkan instance
+createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+createInfo.ppEnabledExtensionNames = extensions.data();
+
+VkInstance instance;
+vkCreateInstance(&createInfo, nullptr, &instance);
