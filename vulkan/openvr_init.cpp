56f5192800da293211f120f85947e2c917f6b46c
boyuia <dodylan190@gmail.com>
2025-02-24 07:59:31 +1100

    Create openvr_init.cpp

---
+++ vulkan/openvr_init.cpp
@@ -0,0 +1,6 @@
+vr::EVRInitError error = vr::VRInitError_None;
+vr::IVRSystem* vrSystem = vr::VR_Init(&error, vr::VRApplication_Scene);
+if (error != vr::VRInitError_None) {
+    printf("Failed to initialize OpenVR\n");
+    return -1;
+}
