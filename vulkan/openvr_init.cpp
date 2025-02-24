e831a64c5d6bc4f3256719a026a1bcac30c09f40
boyuia <dodylan190@gmail.com>
2025-02-25 06:59:30 +1100

    Included header files for Vulkan

--- vulkan/openvr_init.cpp
+++ vulkan/openvr_init.cpp
@@ -1,3 +1,5 @@
+#include <vulkan/vulkan.h>
+
 vr::EVRInitError error = vr::VRInitError_None;
 vr::IVRSystem* vrSystem = vr::VR_Init(&error, vr::VRApplication_Scene);
 if (error != vr::VRInitError_None) {
